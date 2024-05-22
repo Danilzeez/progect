47cd01984ee63c05b9871d4d2ea68c37f8ff84fd
Sutorma <georgsutormin8@gmail.com>
2024-05-23 02:32:25 +0300

    2

--- register.php
+++ register.php
@@ -1,8 +1,43 @@
 <?php
-require_once 'connect.php';
 session_start();
+if ($_SESSION['user']){
+    header('Locatoin: profile.php');
+}
+?>
 
-// Get user input
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Регистрация</title>
+
+</head>
+<body>
+<form action="avtorizup.php" method="post">
+            <div class="form-group">
+                <input type="text" id="name" name="name" placeholder="Ваше Имя">
+                <input type="email" id="email" name="email" placeholder="Ваша электронная почта">
+                <input type="tel" id="number" name="number" placeholder="Ваш номер телефона">
+            </div>
+            <button type="submit">Заказать</button>
+            <?php
+                if ($_SESSION['message']){
+                        echo '<p class=msg>' . $_SESSION['message'] . ' </p>';
+                }
+                unset($_SESSION['message']);
+                
+                ?>
+
+
+        </form>
+</body>
+</html>
+
+
+
+<!-- // Get user input
 $name = $_POST['name'];
 $email = $_POST['email'];
 $number = $_POST['number'];
@@ -11,15 +46,14 @@ $number = $_POST['number'];
 $stmt = $connect->prepare("INSERT INTO `users` (`id`, `name`, `email`, `number`) VALUES (NULL, $name, $email, $number)");
 $stmt->bind_param("sss", $name, $email, $number);
 
-if ($stmt->execute()) {
-    $_SESSION['message'] = "Регистрация прошла успешно!";
-    header('Location: ../register.php');
-} else {
-    $_SESSION['message'] = "Ошибка регистрации: " . $stmt->error;
+
+if 
+    ($_SESSION['message'] = "Ошибка регистрации: " . $stmt->error;
     header('Location: ../register.php');
 }
+    unset($_SESSION['message']);
 
 // Close statement and connection
 $stmt->close();
 $connect->close();
-?>
+?> -->
