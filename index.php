2265dcae089d7abc85f67a5073679b6469625b3b
Sutorma <georgsutormin8@gmail.com>
2024-05-23 01:06:29 +0300

    22

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ if ($_SESSION['user']){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 </head>
 <body>
 <form action="avtorizup.php">
